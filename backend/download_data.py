3d6ae8baa99b773e4ed17c4a4a37d1334a04b49d
nithin p hegde <nithinphegde@gmail.com>
2026-03-15 14:44:49 +0530

    loading transformer first

--- backend/download_data.py
+++ backend/download_data.py
@@ -5,3 +5,8 @@ os.makedirs("data", exist_ok=True)
 hf_hub_download(repo_id="NITHINPhegde/medical-rag-data", filename="medical.index", repo_type="dataset", local_dir="data")
 hf_hub_download(repo_id="NITHINPhegde/medical-rag-data", filename="metadata.json", repo_type="dataset", local_dir="data")
 print("Data downloaded!")
+
+print("Loading embedding model...")
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer("all-MiniLM-L6-v2")
+print("Model ready!")
